90608cadbe1efab8d9f5f41298c6dbec67e1ee32
Orycterope <tvermeilh@gmail.com>
2016-08-25 15:27:48 +0200

    fov

--- src/special_matrixes.c
+++ src/special_matrixes.c
@@ -1,6 +1,9 @@
 #include "fdf.h"
-#define NEAR -10
-#define FAR -100
+#include <math.h>
+#define NEAR 1
+#define FAR 40
+#define FOV_HALF_ANGLE 50
+#define RAD(a) (a * M_PI / 180)
 
 t_matrix_four	*get_camera_offset_matrix()
 {
@@ -13,8 +16,8 @@ t_matrix_four	*get_camera_offset_matrix()
 	while (++i < 4)
 		(*m)[i][i] = 1;
 	// upscale
-	(*m)[0][0] = 500; //
-	(*m)[1][1] = 500; //
+	(*m)[0][0] = WIN_WIDTH; //
+	(*m)[1][1] = WIN_WIDTH; //
 	(*m)[0][3] = WIN_WIDTH / 2;
 	(*m)[1][3] = WIN_HEIGHT / 2;
 	return (m);
@@ -26,12 +29,10 @@ t_matrix_four	*get_perspective_matrix()
 
 	m = (t_matrix_four*)ft_memalloc(sizeof(t_matrix_four));
 	ft_bzero(m, sizeof(t_matrix_four));
-	(*m)[0][0] = 1;
-	(*m)[1][1] = 1;
-	//(*m)[2][2] = (NEAR + FAR) / NEAR;
-	(*m)[2][2] = -1;
-	//(*m)[2][3] = -FAR;
+	(*m)[0][0] = 1 / (tan(RAD(FOV_HALF_ANGLE)));
+	(*m)[1][1] = 1 / (tan(RAD(FOV_HALF_ANGLE)));
+	(*m)[2][2] = -FAR / (FAR - NEAR);
+	(*m)[2][3] = -(FAR * NEAR) / (FAR - NEAR);
 	(*m)[3][2] = -1;
-	//(*m)[3][2] = 1 / NEAR;
 	return (m);
 }

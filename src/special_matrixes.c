09026d5e53ae9e83d596943c23de99149232d8d7
Orycterope <tvermeilh@gmail.com>
2016-08-25 17:13:43 +0200

    perspective matrix takes window ratio into account

--- src/special_matrixes.c
+++ src/special_matrixes.c
@@ -17,7 +17,7 @@ t_matrix_four	*get_camera_offset_matrix()
 		(*m)[i][i] = 1;
 	// upscale
 	(*m)[0][0] = WIN_WIDTH; //
-	(*m)[1][1] = WIN_WIDTH; //
+	(*m)[1][1] = WIN_HEIGHT; //
 	(*m)[0][3] = WIN_WIDTH / 2;
 	(*m)[1][3] = WIN_HEIGHT / 2;
 	return (m);
@@ -26,13 +26,29 @@ t_matrix_four	*get_camera_offset_matrix()
 t_matrix_four	*get_perspective_matrix()
 {
 	t_matrix_four	*m;
+	float			top;
+	float			bottom;
+	float			right;
+	float			left;
 
 	m = (t_matrix_four*)ft_memalloc(sizeof(t_matrix_four));
 	ft_bzero(m, sizeof(t_matrix_four));
-	(*m)[0][0] = 1 / (tan(RAD(FOV_HALF_ANGLE)));
+	right = tan(RAD(FOV_HALF_ANGLE))*NEAR;
+	left = -right;
+	top = right * WIN_HEIGHT / WIN_WIDTH;
+	bottom = -top;
+	(*m)[0][0] = 2 * NEAR / (right - left);
+	(*m)[0][2] = (right + left) / (right - left);
+	(*m)[1][1] = 2 * NEAR / (top - bottom);
+	(*m)[1][2] = (top + bottom) / (top - bottom);
+	(*m)[2][2] = -(FAR + NEAR) / (FAR - NEAR);
+	(*m)[2][3] = -(2 * FAR * NEAR) / (FAR - NEAR);
+	(*m)[3][2] = -1;
+
+	/*(*m)[0][0] = 1 / (tan(RAD(FOV_HALF_ANGLE)));
 	(*m)[1][1] = 1 / (tan(RAD(FOV_HALF_ANGLE)));
 	(*m)[2][2] = -FAR / (FAR - NEAR);
 	(*m)[2][3] = -(FAR * NEAR) / (FAR - NEAR);
-	(*m)[3][2] = -1;
+	(*m)[3][2] = -1;*/
 	return (m);
 }

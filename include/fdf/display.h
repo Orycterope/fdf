efffa3322bb4b38c597f6af60ac97e4aa309871e
Thomas VERMEILH <tvermeil@e2r9p2.42.fr>
2016-11-24 20:03:22 +0100

    apple compile and keycodes

--- include/fdf/display.h
+++ include/fdf/display.h
@@ -4,17 +4,29 @@
 # define WIN_WIDTH 800
 # define WIN_HEIGHT 600
 # define WIN_TITLE "fdf"
-# define KEY_W 119
-# define KEY_S 115
-# define KEY_A 97
-# define KEY_D 100
-# define KEY_I 105
-# define KEY_K 107
-# define KEY_J 106
-# define KEY_L 108
-# define KEY_U 117
-# define KEY_O 111
-
+# ifdef __APPLE__
+#  define KEY_W 13
+#  define KEY_S 1
+#  define KEY_A 0
+#  define KEY_D 2
+#  define KEY_I 34
+#  define KEY_K 40
+#  define KEY_J 38
+#  define KEY_L 37
+#  define KEY_U 32
+#  define KEY_O 31
+# else
+#  define KEY_W 119
+#  define KEY_S 115
+#  define KEY_A 97
+#  define KEY_D 100
+#  define KEY_I 105
+#  define KEY_K 107
+#  define KEY_J 106
+#  define KEY_L 108
+#  define KEY_U 117
+#  define KEY_O 111
+# endif
 typedef struct		s_display
 {
 	void			*mlx_ptr;
